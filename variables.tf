0bd6a62573cc500ab31f6e8ce3e42d26c27e750e
Brandon <brandon.neo@gmail.com>
2019-07-31 14:36:13 +1000

    amendments

--- variables.tf
+++ variables.tf
@@ -1,10 +1,15 @@
 //  The region we will deploy our cluster into.
 variable "region" {
   description = "Region to deploy the cluster into"
-  default = "us-east-1"
+  default = "ap-southeast-2"
 }
 
 //  The public key to use for SSH access.
 variable "public_key_path" {
-  default = "~/.ssh/id_rsa.pub"
+  default = "~/.ssh/ocp.pub"
 }
+
+variable "ocp_user" {
+  default = "neo"
+}
+
